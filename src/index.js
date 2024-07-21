affa5bb11fc945d0c894cfc700294d660fe796dd
Andrew Martinez <andy72391@gmail.com>
2024-07-20 20:54:36 -0400

    Added props for components and styling

--- src/index.js
+++ src/index.js
@@ -1,22 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1>React App</h1>
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
   return (
-    <div>
-      <img src="pizzas/margherita.jpg" alt="Pizza margherita" />
-      <h2>Pizza Margherita</h2>
-      <p>Tomato and mozarella</p>
-      <p>Price: $10</p>
+    <header className="header">
+      <h1>React Pizza</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Menu</h2>
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Tomato and mozarella"
+        price={10}
+        photoName="pizzas/margherita.jpg"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={13}
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}, We are currently open!
+    </footer>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>
+          <p>{props.price}</p>
+        </span>
+      </div>
     </div>
   );
 }
